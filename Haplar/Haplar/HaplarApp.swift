b2265d328fce7020d29af92eba08a24f9b0c8907
TrabelsiAchraf <trabelsiachraf.mobile@gmail.com>
2021-02-09 21:06:13 +0100

    init project

---
+++ Haplar/Haplar/HaplarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaplarApp.swift
+//  Haplar
+//
+//  Created by Achraf Trabelsi on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HaplarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
