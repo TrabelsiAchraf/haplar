374fd435acf701d98903f5b989ac0d626025cc50
TrabelsiAchraf <achraf.trabelsi@engie.com>
2021-03-15 23:26:16 +0100

    add test for Stock Status model

--- Haplar/Haplar/Application/Features/Home/Model/MedicineShceduleModel.swift
+++ Haplar/Haplar/Application/Features/Home/Model/MedicineShceduleModel.swift
@@ -15,9 +15,25 @@ struct MedicineShcedule: Identifiable {
     var leftUnit: String {
         "\(quanity.left)/\(quanity.total) units"
     }
+    var leftUnitStatus: StockStatus {
+        let half = quanity.total * 50 / 100
+        let minConfortable = quanity.total * 70 / 100
+        let third = quanity.total * 30 / 100
+        if quanity.left >= half && quanity.left <= minConfortable {
+            return .normal
+        } else if quanity.left <= third {
+            return .risk
+        } else {
+            return .comfortable
+        }
+    }
     
     struct Quanity {
         let total: Int
         let left: Int
+    }
+    
+    enum StockStatus {
+        case risk, normal, comfortable
     }
 }
--- Haplar/HaplarTests/Home/Model/MedicineShceduleTests.swift
+++ Haplar/HaplarTests/Home/Model/MedicineShceduleTests.swift
@@ -11,9 +11,21 @@ import SwiftUI
 
 class MedicineShceduleTests: XCTestCase {
     
-    func test_thatfails() {
+    func test_leftUnit_shouldRespectLeftByTotalUnitsFormat() {
         let quantity = MedicineShcedule.Quanity(total: 40, left: 20)
         let sut = MedicineShcedule(id: UUID(), icon: Image("test"), name: "Arnabite", quanity: quantity)
         XCTAssertEqual(sut.leftUnit, "20/40 units", "left unit message should respect the format: 'left/total units'")
+    }
+    
+    func test_ifLeftQuantityIsHalfTotal_gaugeShouldNormalStatus() {
+        let quantity = MedicineShcedule.Quanity(total: 40, left: 21)
+        let sut = MedicineShcedule(id: UUID(), icon: Image("test"), name: "Arnabite", quanity: quantity)
+        let quantity2 = MedicineShcedule.Quanity(total: 40, left: 25)
+        let sut2 = MedicineShcedule(id: UUID(), icon: Image("test"), name: "Arnabite", quanity: quantity2)
+        let quantity3 = MedicineShcedule.Quanity(total: 40, left: 27)
+        let sut3 = MedicineShcedule(id: UUID(), icon: Image("test"), name: "Arnabite", quanity: quantity3)
+        XCTAssertEqual(sut.leftUnitStatus, .normal)
+        XCTAssertEqual(sut2.leftUnitStatus, .normal)
+        XCTAssertEqual(sut3.leftUnitStatus, .normal)
     }
 }
